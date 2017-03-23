48fac39651472e26b23e6465fcf406ff38734a43
truechuan <chen_chuan@126.com>
2017-03-23 17:57:21 +0800

    爬取汽车之家论坛数据

---
+++ autohome_club/pipelines.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+
+import json
+import codecs
+
+
+class AutohomeClubPipeline(object):
+    def __init__(self):
+        self.file = codecs.open('XC60.json', 'wb', encoding='utf-8')
+
+    def process_item(self, item, spider):
+        if item['content'].strip() != "":
+            line = json.dumps(dict(item)) + '\n'
+            self.file.write(line.decode("unicode_escape"))
+        return item
---
+++ autohome_club/spiders/spider.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@author: chenchuan@autohome.com.cn
+@time: 2017/03/13
+"""
+# scrapy crawl XC60 --set LOG_FILE=log
+
+
+from scrapy.spider import BaseSpider
+from scrapy.selector import Selector
+import sys
+from scrapy.http import Request
+from autohome_club.items import AutohomeClubItem
+
+default_encoding = 'utf-8'
+domain = 'http://club.autohome.com.cn'
+if sys.getdefaultencoding() != default_encoding:
+    reload(sys)
+    sys.setdefaultencoding(default_encoding)
+
+
+class AutohomeSpider(BaseSpider):
+    name = "XC60"
+    allowed_domains = ["autohome.com.cn"]
+
+    start_urls = [
+        "http://club.autohome.com.cn/bbs/forum-c-3411-1.html"
+    ]
+
+    def completeUrl(self, _url):
+        if not _url.startswith('http', 0, 4):
+            _url = domain + _url
+        return _url
+
+    # 车系列表页
+    def parse(self, response):
+        f = open('urls.txt', 'wb')
+        hxs = Selector(response)
+        urls = hxs.xpath('//div[@id="subcontent"]//a[@class="a_topic"]/@href')
+        # for url in urls:
+        #     _url = self.completeUrl(url.extract())
+        #
+        #     f.write(_url + "\n")
+        #     yield Request(_url, callback=self.parse_item)
+        # 车系列表页分页
+        page = hxs.xpath('//div[@class="pages"]//a[@class="afpage"]/@href')
+        if page:
+            next_page = self.completeUrl(page.extract()[0])
+            f.write(next_page + "\n")
+            yield Request(next_page, callback=self.parse)
+
+    # 车系列表页 -> 论坛讨论页面
+    def parse_item(self, response):
+        items = []
+        link = response.url
+
+        hxs = Selector(response)
+        title = hxs.xpath('//title/text()').extract()
+        title = title[0].decode(default_encoding)
+
+        contents = hxs.xpath('//div[contains(@class, "clearfix contstxt outer-section")]')
+        f = open('log.txt', 'wb')
+
+        for index, con in enumerate(contents):
+            item = AutohomeClubItem()
+
+            try:
+                _text = self.getContext(con)
+
+                _user_name = con.xpath('.//a[@xname="uname"]/text()').extract()[0]
+                _user_name.decode(default_encoding)
+
+                _date = con.xpath('.//span[@xname="date"]/text()').extract()[0]
+
+                item['title'] = title
+                item['link'] = link
+                item['uname'] = _user_name
+                item['content'] = _text
+                item['date'] = _date
+                items.append(item)
+            except BaseException:
+                print f.write(con.extract() + "\n")
+                print f.write("==================>>>>>>>>>>>>>>><<<<<<<<<<<<<=================\n")
+            yield item
+
+            # filename = response.url.split("/")[-1]
+            # with open(filename, 'wb') as f:
+            #     f.write(response.body)
+
+    def getContext(self, con):
+        _text = con.xpath('.//div[@class="w740"]/text()').extract()[0]
+        _text.decode(default_encoding)
+        return _text
